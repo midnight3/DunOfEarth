5698de2219bfc96cba585fe959ba7220b733a4a5
Sarah Welsch <sarahwelsch@Sarahs-MacBook-Pro.local>
2017-10-16 14:25:34 -0500

    Added enimy prefab, change camera position, added empty gameobject for floor

--- Dungeons Of Earth/Assets/gameHandler.cs
+++ Dungeons Of Earth/Assets/gameHandler.cs
@@ -7,12 +7,13 @@ public class gameHandler : MonoBehaviour {
 	public GameObject floorPrefab;
 	public GameObject wallPrefab;
 	public GameObject playerPrefab;
+	public GameObject parentTerrain;
 
 	// Use this for initialization
 	void Start () {
 		for (float i = 0f; i < 10f; i++) {
 			for (float j = 0f; j < 10f; j++) {
-				Instantiate (floorPrefab, new Vector3 (i, j), Quaternion.identity);
+				Instantiate (floorPrefab, new Vector3 (i, j), Quaternion.identity, parentTerrain.transform);
 			}
 		}
 		Instantiate (playerPrefab, new Vector3 (5.0f, 5.0f), Quaternion.identity);
